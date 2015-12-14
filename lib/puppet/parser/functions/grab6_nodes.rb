4bdddb9782a56362ca5902269b8a489c72183b5a
olo10 <shegoj@yahoo.com>
2015-12-14 19:14:41 +0000

    testing

--- lib/puppet/parser/functions/grab6_nodes.rb
+++ lib/puppet/parser/functions/grab6_nodes.rb
@@ -2,10 +2,8 @@ module Puppet::Parser::Functions
 	newfunction(:grab3_nodes, :type => :rvalue) do |args|
 		#rand(args.empty? ? 0 : args[0])
 		 nodes = args [0]
-	   nodes.each_index do |i|
-				nodes[i]
-				i
-     end
+		 "shegoj"
+     	
   end
 end
 

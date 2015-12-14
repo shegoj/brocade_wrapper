d3728a0c4b20314ee105ba8768d6b58bdcef3a46
olo10 <shegoj@yahoo.com>
2015-12-14 18:39:48 +0000

    testing

--- lib/puppet/parser/functions/grab3_nodes.rb
+++ lib/puppet/parser/functions/grab3_nodes.rb
@@ -1,5 +1,5 @@
 module Puppet::Parser::Functions
-	newfunction(:grab2_nodes, :type => :rvalue) do |args|
+	newfunction(:grab3_nodes, :type => :rvalue) do |args|
 		#rand(args.empty? ? 0 : args[0])
 	   for number in args[0]
 				#{number}
